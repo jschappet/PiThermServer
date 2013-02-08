afb6760eaa3f89195b83f5836910a8a1c612659c
James Schappet <jimmy@schappet.com>
2013-02-08 09:07:53 -0600

    moving time out of the loop

--- getTemp.pl
+++ getTemp.pl
@@ -4,16 +4,17 @@ use Time::Piece;
 
 @FILES = glob "/sys/bus/w1/devices/*/w1_slave";
 
+$t = Time::Piece->new;
+$time = $t->epoch;
 foreach (@FILES) {
 
 	$fileName=$_;
 	open(T1, $fileName);
 	my($devName) = $fileName =~ m%/sys/bus/w1/devices/(.*)/w1_slave%;
-	$t = Time::Piece->new;
 	while(<T1>) {
 		if (/t=([\d|-]*)$/) {
 			$cTemp = sprintf("%.2f", $1 / 1000);
-			print ",{\"temperature_record\":[{\"unix_time\":".$t->epoch.
+			print ",{\"temperature_record\":[{\"unix_time\":".$time.
 				",\"celsius\":$cTemp,\"device\":\"".$devName."\"}]}\n";
 		}
 	}
